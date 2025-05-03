9266c2f7413a0193ed5a4c43c01b40ee88c68249
Alex <alex.varna.91@gmail.com>
2025-05-03 12:09:02 +0200

    Update password encoder

--- src/main/java/com/MyTutor2/config/ApplicationBeanConfiguration.java
+++ src/main/java/com/MyTutor2/config/ApplicationBeanConfiguration.java
@@ -3,6 +3,7 @@ package com.MyTutor2.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
@@ -10,15 +11,16 @@ import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 public class ApplicationBeanConfiguration {
 
     @Bean
-    public ModelMapper modelMapper(){
+    public ModelMapper modelMapper() {
         return new ModelMapper();
     }
 
     //SpringSecurity_3
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return Pbkdf2PasswordEncoder
-                .defaultsForSpringSecurity_v5_8();
+        //return Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+        //we update the password encoder for maximum flexibility. This supports multiple formats (bcrypt, pbkdf2, scrypt, etc.)
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 }
