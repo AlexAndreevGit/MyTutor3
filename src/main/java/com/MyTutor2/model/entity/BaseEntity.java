6a6b27088d8fef937459b80557ba563aa1a7ac21
Alex <alex.varna.91@gmail.com>
2025-07-19 13:31:14 +0200

    update BaseEntity.java with lombok

--- src/main/java/com/MyTutor2/model/entity/BaseEntity.java
+++ src/main/java/com/MyTutor2/model/entity/BaseEntity.java
@@ -5,7 +5,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @MappedSuperclass
 public abstract class BaseEntity {
 
@@ -13,12 +17,4 @@ public abstract class BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
