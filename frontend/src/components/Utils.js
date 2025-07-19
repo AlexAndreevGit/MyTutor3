077d04755491d46e9535af0fb9c746b7cafc1f4f
Alex <alex.varna.91@gmail.com>
2025-07-19 12:46:26 +0200

    Update CSRF

--- frontend/src/components/Utils.js
+++ frontend/src/components/Utils.js
@@ -24,3 +24,29 @@ export function addCsrfTokenToForm(elementId) {
         })
         .catch(error => console.error("Could not fetch CSRF token:", error));
 }
+
+
+export function sentFormWithCSRFToURL(URL) {
+    const form = document.createElement('form');
+    form.method = 'POST';
+    form.action = URL;
+
+    // Add CSRF token if available
+    fetch('/api/csrf')
+        .then(response => response.json())
+        .then(data => {
+            if (data.token) {
+                const csrfInput = document.createElement('input');
+                csrfInput.type = 'hidden';
+                csrfInput.name = data.parameterName;
+                csrfInput.value = data.token;
+                form.appendChild(csrfInput);
+
+                document.body.appendChild(form);
+                form.submit();
+            }
+        })
+        .catch(error => {
+            console.error('Could not fetch CSRF token for logout:', error);
+        });
+}
